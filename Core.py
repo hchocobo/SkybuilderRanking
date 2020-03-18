7a0f4ea511186eaecab67042a00f948b1b75a135
Charles Liang <hchocobo@gmail.com>
2020-03-18 16:59:38 +0800

    CSV

--- Core.py
+++ Core.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import datetime
 
 class SkybuilderRankPlayer:
     def __init__(self,dc,server,rank,rank_change,name,fc,score,score_change):
@@ -52,58 +53,90 @@ class SkybuilderCrawler:
                 #r_ranklist.append(SkybuilderRankPlayer(row[6],row[7],row[0],row[1],row[2],row[3],row[4],row[5]))
                 print(row[0]," ", row[1], " ", row[2], " ", row[3], " ", row[4], " ", row[5], " ", row[6], " ", row[7])
 
+class ffxiv_world:
+    def __init__(self, dc, world, region):
+        self.dc = dc
+        self.world = world
+        self.region = region
+ff_job = ('carpenter','blacksmith','armorer','goldsmith','leatherworker','weaver','alchemist','culinarian','miner','botanist','fisher')
+
+jp_datacenter = ('Elemental', 'Gaia', 'Mana')
+na_datacenter = ('Aether', 'Primal', 'Crystal')
+eu_datacenter = ('Chaos', 'Light')
+
+jp_worlds = (('Aegis','Atomos','Carbuncle','Garuda','Gungnir','Kujata','Ramuh','Tonberry','Typhon','Unicorn'), ('Alexander', 'Bahamut', 'Durandal', 'Fenrir', 'Ifrit', 'Ridill', 'Tiamat', 'Ultima', 'Valefor', 'Yojimbo', 'Zeromus'), ('Anima', 'Asura', 'Belias', 'Chocobo', 'Hades', 'Ixion', 'Mandragora', 'Masamune', 'Pandaemonium', 'Shinryu','Titan'))
+na_worlds = (('Adamantoise', 'Cactuar', 'Faerie', 'Gilgamesh', 'Jenova', 'Midgardsormr', 'Sargatanas', 'Siren'),('Behemoth', 'Excalibur', 'Exodus', 'Famfrit', 'Hyperion', 'Lamia', 'Leviathan', 'Ultros'),('Balmung', 'Brynhildr', 'Coeurl', 'Diabolos', 'Goblin', 'Malboro', 'Mateus', 'Zalera'))
+eu_worlds = (('Cerberus', 'Louisoix', 'Moogle', 'Omega', 'Ragnarok', 'Spriggan'),('Lich', 'Odin', 'Phoenix', 'Shiva', 'Twintania', 'Zodiark'))
 
 
 
+def url_build(region, datacenter, world, job):
+# Default skybuilder ranking string
+    default_url = "finalfantasyxiv.com/lodestone/ishgardian_restoration/ranking/"
 
+    # Check Region default to jp region
+    region = region.lower()
+    if region != 'jp' and region != 'na' and region != 'eu':
+        region = 'jp'
 
-
-def url_build(datacenter, world, job):
-    # ('Elemental', 'Mana', 'Gaia') 3
-    # ('Aegis','Atomos','Carbuncle','Garuda','Gungnir','Kujata','Ramuh','Tonberry','Typhon','Unicorn')
-    # ('Alexander','Bahamut','Durandal','Fenrir','Ifrit','Ridill','Tiamat,Ultima','Valefor','Yojimbo','Zeromus')
-    # ('Anima', 'Asura', 'Belias', 'Chocobo', 'Hades', 'Ixion', 'Mandragora', 'Masamune', 'Pandaemonium', 'Shinryu','Titan')
-
-    # ('Aether', 'Primal', 'Crystal') 3
-    # ('Adamantoise', 'Cactuar', 'Faerie', 'Gilgamesh', 'Jenova', 'Midgardsormr', 'Sargatanas', 'Siren') 8
-    # ('Behemoth', 'Excalibur', 'Exodus', 'Famfrit', 'Hyperion', 'Lamia', 'Leviathan', 'Ultros') 8
-    # ('Balmung', 'Brynhildr', 'Coeurl', 'Diabolos', 'Goblin', 'Malboro', 'Mateus', 'Zalera') 8
-
-    # ('Chaos', 'Light') 3
-    # ('Cerberus', 'Louisoix', 'Moogle', 'Omega', 'Ragnarok', 'Spriggan') 6
-    # ('Lich', 'Odin', 'Phoenix', 'Shiva', 'Twintania', 'Zodiark') 6
-
-    # class, worldname, dcgroup
-    default_url = 'na.finalfantasyxiv.com/lodestone/ishgardian_restoration/ranking/'
-    ff_job = ('carpenter','blacksmith','armorer','goldsmith','leatherworker','weaver','alchemist','culinarian','miner','botanist','fisher')
-    ff_dcgroup = ('Elemental','Gaia','Mana')
-    ff_elemental_world = ('Aegis','Atomos','Carbuncle','Garuda','Gungnir','Kujata','Ramuh','Tonberry','Typhon','Unicorn')
-    ff_gaia_world = ('Alexander','Bahamut','Durandal','Fenrir','Ifrit','Ridill','Tiamat,Ultima','Valefor','Yojimbo','Zeromus')
-    ff_mana_world = ('Anima', 'Asura', 'Belias', 'Chocobo', 'Hades', 'Ixion', 'Mandragora', 'Masamune', 'Pandaemonium', 'Shinryu','Titan')
-
-    jp_datacenter = (('Aegis','Atomos','Carbuncle','Garuda','Gungnir','Kujata','Ramuh','Tonberry','Typhon','Unicorn'), ('Alexander','Bahamut','Durandal','Fenrir','Ifrit','Ridill','Tiamat,Ultima','Valefor','Yojimbo','Zeromus'), ('Anima', 'Asura', 'Belias', 'Chocobo', 'Hades', 'Ixion', 'Mandragora', 'Masamune', 'Pandaemonium', 'Shinryu','Titan'))
-
-    r_job = ff_job[job] + '?'
-    dcgroup = 'dcgroup='+ ff_dcgroup[datacenter] + '#ranking'
-
-    if datacenter == 0:
-        worldname = 'worldname=' + ff_elemental_world[world] + '&'
-    elif datacenter == 1:
-        worldname = 'worldname=' + ff_gaia_world[world] + '&'
-    elif datacenter == 2:
-        worldname = 'worldname=' + ff_mana_world[world] + '&'
+    # Setup Datacenter
+    if region != 'eu':
+        max_datacenter = 2
     else:
-        worldname = ''
+        max_datacenter = 1
 
-    default_url = "https://" + default_url + r_job + worldname + dcgroup
+    # Datacenter is out of range set it to 0
+    if datacenter > max_datacenter:
+        datacenter = 0
+
+    # Setup Current datacenter
+    if region == 'na':
+        cur_dc = na_datacenter[datacenter]
+    elif region == 'eu':
+        cur_dc = eu_datacenter[datacenter]
+    else:
+        cur_dc = jp_datacenter[datacenter]
+
+    # Setup Max World Limit
+    if region == 'na':
+        max_world = len(na_worlds[datacenter])
+    elif region == 'eu':
+        max_world = len(eu_worlds[datacenter])
+    else:
+        max_world = len(jp_worlds[datacenter])
+
+    # default to first world when max out
+    if world >= max_world:
+        world = 0
+    # setup current world
+    if region == 'na':
+        cur_world = na_worlds[datacenter][world]
+    elif region == 'eu':
+        cur_world = eu_worlds[datacenter][world]
+    else:
+        cur_world = jp_worlds[datacenter][world]
+
+    # Check job range default to 0
+    if job >= len(ff_job):
+        job = 0
+    #setup Job
+    cur_job = ff_job[job]
+
+    # build URL https://na.finalfantasyxiv.com/lodestone/ishgardian_restoration/ranking/ carpenter?worldname=Aegis&dcgroup=Elemental#ranking
+    region = 'na'
+    default_url = "https://" + region + "." + default_url + cur_job + "?worldname=" + cur_world + "&dcgroup=" + cur_dc + "#ranking"
 
     return default_url
 
-def build_ranklist(current_ranklist,dc,world):
+def build_ranklist(current_ranklist, r_dc, r_world):
+    print("DC: ", r_dc)
+    print("World: ", r_world)
     rank_list = []
     for rank_character in current_ranklist:
+        # get rank
         r_rank = rank_character.find('p',{'class': 'ranking-order'}).string
 
+        # get rank change
         if rank_character.find('p',{'class': 'ranking-prev_order__rank_down'}):
             r_rankChange = '-' + rank_character.find('p',{'class': 'ranking-prev_order__rank_down'}).string
         elif rank_character.find('p',{'class': 'ranking-prev_order__rank_up'}):
@@ -111,39 +144,60 @@ def build_ranklist(current_ranklist,dc,world):
         else:
             r_rankChange = '0'
 
+        # get character name
         r_name = rank_character.find('div',{'class': 'ranking-name'}).find('p').string
-
+        # get fc name
         if not rank_character.find('div', {'class': 'ranking-fc'}):
             r_fc = ' '
         else:
             r_fc = rank_character.find('div', {'class': 'ranking-fc'}).find('p').string
-
+        # get score
         r_score = rank_character.find('div', {'class': 'ranking-score'}).find('p').string
-
+        # get score change
         if rank_character.find('div', {'class': 'ranking-score'}).find('span'):
             r_scorechange = rank_character.find('div', {'class': 'ranking-score'}).find('span').string
         else:
             r_scorechange = '0'
+        # send data into skybuilder obj
+        rank_list.append(SkybuilderRankPlayer(_dc,_world,r_rank,r_rankChange,r_name,r_fc,r_score,r_scorechange))
 
-        #dc,server,rank,rank_change,name,fc,score,score_change):
-        rank_list.append(SkybuilderRankPlayer(dc,world,r_rank,r_rankChange,r_name,r_fc,r_score,r_scorechange))
         t_ranklist = tuple(rank_list)
-        print(r_rank,' (', r_rankChange,') ', r_name,' |', r_fc, '| ', r_score, ' (', r_scorechange, ')')
-    return t_ranklist
+       # print(r_rank,' (', r_rankChange,') ', r_name,' |', r_fc, '| ', r_score, ' (', r_scorechange, ')')
+
+    return
+
+# URL LOOP for datacenter
+r_region = 'jp'
+r_dc = 0
+r_world = 0
+r_job = 0
+
+while (r_world < 10):
+    print("R_WORLD: ", r_world )
+
+    while (r_job < 12):
+       url = url_build(r_region, r_dc, r_world, r_job)
+
+       # Grab ranking page
+       source_data = requests.get(url)
+       soup = BeautifulSoup(source_data.text, 'lxml')
+       # Grab top 12
+       all_top12 = soup.find('div', {'class': 'ranking-soyf'})
+       # Grab remaining 100
+       all_top100 = soup.find('div', {'class': 'ranking-wrapper'})
+       # build rank list
+       c_ranklist = all_top12.find_all('li', {'class': 'ranking-list__item'})
+       c_ranklist += all_top100.find_all('li', {'class': 'ranking-list__item'})
+       c_ranklist = build_ranklist(c_ranklist, jp_datacenter[r_dc], jp_worlds[r_dc][r_world])
+       print(c_ranklist)
+       r_job = r_job + 1
+
+    r_job = 0
+    #r_world = r_world + 1
 
 
-
-url = url_build(0,0,0)
-source_data = requests.get(url)
-
-soup = BeautifulSoup(source_data.text,'lxml')
-
-all_top12 = soup.find('div',{'class': 'ranking-soyf'})
-all_top100 = soup.find('div',{'class': 'ranking-wrapper'})
-
-c_ranklist = all_top12.find_all('li', {'class': 'ranking-list__item'})
-c_ranklist += all_top100.find_all('li', {'class': 'ranking-list__item'})
-c_ranklist = build_ranklist(c_ranklist,"Elemental","Ageis")
+""""
+    
 print("TESTING")
 
 for r in c_ranklist:
@@ -152,3 +206,4 @@ for r in c_ranklist:
 character = SkybuilderCrawler(c_ranklist)
 character.SkybuilderCSVWrite()
 character.SkybuilderCSVRead()
+"""""
--- TEST.py
+++ TEST.py
@@ -1,26 +1,69 @@
-def url_build(datacenter, world, job):
-    # class, worldname, dcgroup
-    default_url = 'na.finalfantasyxiv.com/lodestone/ishgardian_restoration/ranking/'
+def url_build(region, datacenter, world, job):
+# Default skybuilder ranking string
+    default_url = "na.finalfantasyxiv.com/lodestone/ishgardian_restoration/ranking/"
     ff_job = ('carpenter','blacksmith','armorer','goldsmith','leatherworker','weaver','alchemist','culinarian','miner','botanist','fisher')
-    ff_dcgroup = ('Elemental','Gaia','Mana')
-    ff_elemental_world = ('Aegis','Atomos','Carbuncle','Garuda','Gungnir','Kujata','Ramuh','Tonberry','Typhon','Unicorn')
-    ff_gaia_world = ('Alexander','Bahamut','Durandal','Fenrir','Ifrit','Ridill','Tiamat,Ultima','Valefor','Yojimbo','Zeromus')
-    ff_mana_world = ('Anima', 'Asura', 'Belias', 'Chocobo', 'Hades', 'Ixion', 'Mandragora', 'Masamune', 'Pandaemonium', 'Shinryu','Titan')
+    jp_datacenter = ('Elemental', 'Gaia', 'Mana')
+    na_datacenter = ('Aether', 'Primal', 'Crystal')
+    eu_datacenter = ('Chaos', 'Light')
+    jp_worlds = (('Aegis','Atomos','Carbuncle','Garuda','Gungnir','Kujata','Ramuh','Tonberry','Typhon','Unicorn'), ('Alexander', 'Bahamut', 'Durandal', 'Fenrir', 'Ifrit', 'Ridill', 'Tiamat', 'Ultima', 'Valefor', 'Yojimbo', 'Zeromus'), ('Anima', 'Asura', 'Belias', 'Chocobo', 'Hades', 'Ixion', 'Mandragora', 'Masamune', 'Pandaemonium', 'Shinryu','Titan'))
+    na_worlds = (('Adamantoise', 'Cactuar', 'Faerie', 'Gilgamesh', 'Jenova', 'Midgardsormr', 'Sargatanas', 'Siren'),('Behemoth', 'Excalibur', 'Exodus', 'Famfrit', 'Hyperion', 'Lamia', 'Leviathan', 'Ultros'),('Balmung', 'Brynhildr', 'Coeurl', 'Diabolos', 'Goblin', 'Malboro', 'Mateus', 'Zalera'))
+    eu_worlds = (('Cerberus', 'Louisoix', 'Moogle', 'Omega', 'Ragnarok', 'Spriggan'),('Lich', 'Odin', 'Phoenix', 'Shiva', 'Twintania', 'Zodiark'))
+# Error checking
+    # Check Region default to jp region
+    region = region.lower()
+    if region != 'jp' or region != 'na' or region != 'eu':
+        region = 'jp'
 
-    r_job = ff_job[job] + '?'
-    dcgroup = 'dcgroup='+ ff_dcgroup[datacenter] + '#ranking'
-
-    if datacenter == 0:
-        worldname = 'worldname=' + ff_elemental_world[world] + '&'
-    elif datacenter == 1:
-        worldname = 'worldname=' + ff_gaia_world[world] + '&'
-    elif datacenter == 2:
-        worldname = 'worldname=' + ff_mana_world[world] + '&'
+    # Setup Datacenter
+    if region != 'eu':
+        max_datacenter = 2
     else:
-        worldname = ''
+        max_datacenter = 1
 
-    default_url = "https://" + default_url + r_job + worldname + dcgroup
+    # Datacenter is out of range set it to 0
+    if datacenter > max_datacenter:
+        datacenter = 0
+
+    # Setup Current datacenter
+    if region == 'na':
+        cur_dc = na_datacenter[datacenter]
+    elif region == 'eu':
+        cur_dc = eu_datacenter[datacenter]
+    else:
+        cur_dc = jp_datacenter[datacenter]
+
+    # Setup Max World Limit
+    if region == 'na':
+        max_world = len(na_worlds[datacenter])
+    elif region == 'eu':
+        max_world = len(eu_worlds[datacenter])
+    else:
+        max_world = len(jp_worlds[datacenter])
+    print("MaxWorld: ", max_world)
+
+    # Define datacenter and world
+    if region == 'jp':
+        # Default DC Elemental
+        if datacenter <= 2:
+            c_datacenter = jp_datacenter[datacenter]
+            max_world = jp_worlds[datacenter]
+        else:
+            c_datacenter = jp_datacenter[0]
+            max_world = jp_worlds[datacenter]
+
+        c_world = jp_worlds[datacenter][world]
+    elif region == 'na':
+        c_datacenter = na_datacenter[datacenter]
+        c_world = na_worlds[datacenter][world]
+    elif region == 'eu':
+        c_datacenter = eu_datacenter[datacenter]
+        c_world = eu_worlds[datacenter][world]
+    else:
+        c_datacenter = jp_datacenter[datacenter]
+        c_world = jp_worlds[datacenter][world]
+
+    print("Datacenter: ", c_datacenter)
+    print("World: ", c_world)
 
     return default_url
-
 
